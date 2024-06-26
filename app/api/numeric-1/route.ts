2317c661ec0c6c4b5a8602bfcd3823a1d6989573
oji3 <oji3xx@gmail.com>
2024-06-26 22:01:30 +0800

    fix

---
+++ app/api/numeric-1/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest } from "next/server";
+import { isAddress } from "viem";
+import { createSignature } from "@/lib/signature";
+
+export async function GET(req: NextRequest, { params: { chainId } }: { params: { chainId: string } }) {
+  try {
+    const address = req.nextUrl.searchParams.get("address");
+    if (!address || !isAddress(address)) {
+      return new Response("Invalid address", { status: 400 });
+    }
+
+    const result = true;
+    const data = (100).toString();
+
+    const signature = await createSignature({ address, result, data });
+
+    return Response.json({ signature, result, data }, { status: 200 });
+  } catch (err) {
+    console.error(err);
+    return new Response("Internal server error", { status: 500 });
+  }
+}
